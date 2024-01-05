7aed833c24e32fed876bb94babf987e478c226bd
George Gesite <georgesite33@gmail.com>
2024-01-06 07:50:09 +0800

    Learning TypeScript 1

---
+++ tsbasics/index.js
@@ -0,0 +1,8 @@
+var fullname = "John Doe";
+console.log(fullname);
+var variableA = 10;
+var variableB = 20;
+console.log(variableA + variableB);
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
---
+++ tsbasics/index.ts
@@ -0,0 +1,11 @@
+let fullname: string = "John Doe";
+console.log(fullname);
+
+let variableA : number = 10;
+let variableB : number = 20;
+
+console.log(variableA + variableB);
+
+for(let i:number = 0; i < 10; i++){
+    console.log(i);
+}
