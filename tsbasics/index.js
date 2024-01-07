152c08dcbfa403afa24c5ef7706d117cb41f1ed5
George Gesite <georgesite33@gmail.com>
2024-01-07 09:01:52 +0800

    Learning Basics

--- tsbasics/index.js
+++ tsbasics/index.js
@@ -1,8 +1,28 @@
-var fullname = "John Doe";
-console.log(fullname);
-var variableA = 10;
-var variableB = 20;
-console.log(variableA + variableB);
-for (var i = 0; i < 10; i++) {
+// String Variable Type
+var firstName = "George";
+var lastName = "Washington";
+console.log(firstName + " " + lastName);
+// for loops
+for (var i = 0; i < 5; i++) {
     console.log(i);
 }
+// functions
+function addition(a, b) {
+    return a + b;
+}
+console.log(addition(10, 20));
+// array
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+// tuple
+var tuple = ["George", 10];
+console.log(tuple[0]);
+// objects
+var person = {
+    firstName: "George",
+    lastName: "Washington",
+    age: 10
+};
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
--- tsbasics/index.ts
+++ tsbasics/index.ts
@@ -1,11 +1,37 @@
-let fullname: string = "John Doe";
-console.log(fullname);
+// String Variable Type
+let firstName: string = "George";
+let lastName: string = "Washington";
+console.log(firstName + " " + lastName);
 
-let variableA : number = 10;
-let variableB : number = 20;
-
-console.log(variableA + variableB);
-
-for(let i:number = 0; i < 10; i++){
+// for loops
+for(let i:number = 0; i < 5; i++){
     console.log(i);
 }
+// functions
+function addition(a:number, b:number):number{  
+    return a + b;
+}
+console.log(addition(10, 20));
+
+// array
+let arr: number[] = [1, 2, 3, 4, 5];
+console.log(arr);
+
+// tuple
+let tuple: [string, number] = ["George", 10];
+console.log(tuple[0]);
+
+// objects
+let person: {
+    firstName: string,
+    lastName: string,
+    age: number
+} = {
+    firstName: "George",
+    lastName: "Washington",
+    age: 10
+}
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
