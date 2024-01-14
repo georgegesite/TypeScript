9d814f75efdf16ad9e7980ca29af71191230ffa4
George Gesite <georgesite33@gmail.com>
2024-01-14 10:55:57 +0800

    Implemented NavBar

--- react + ts/vite-project/src/main.tsx
+++ react + ts/vite-project/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Navbar from './portfolio/Navbar.tsx'
+import "./css/Style.css";
+import "./css/MediaQueries.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
 )
