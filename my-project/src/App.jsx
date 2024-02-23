dc6a75d9115c8da614d1c32f703e55159c85a5c1
George Gesite <georgesite33@gmail.com>
2024-02-23 22:21:28 +0800

    Update App.jsx

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-      Hello world!
+      This is a testing
     </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + count * 2)}>
