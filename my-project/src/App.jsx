293b3a4b0b92647e06480aea1e391b4c5c760f6e
George Gesite <georgesite33@gmail.com>
2024-03-02 07:36:01 +0800

    update

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -8,8 +8,8 @@ export default function App() {
       <h1 className="text-3xl font-bold underline">
       This is a testing
     </h1>
-      <div className="border-2 border-red-500 button">
-        <button onClick={() => setCount((count) => count + count * 2)}>
+      <div >
+        <button className ="bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCount((count) => count + count * 2)}>
           count is {count}
         </button>
       </div>
