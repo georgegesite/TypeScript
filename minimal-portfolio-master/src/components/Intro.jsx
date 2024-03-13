ffe85394a031492585d637be34f39ca2292913e3
George Gesite <georgesite33@gmail.com>
2024-03-13 21:20:18 +0800

    Update Intro.jsx

--- minimal-portfolio-master/src/components/Intro.jsx
+++ minimal-portfolio-master/src/components/Intro.jsx
@@ -3,26 +3,23 @@ import React from 'react';
 function Intro() {
    return (
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Forrest</h1>
-         <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
+         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">George Gesite</h1>
+         <p className="text-base md:text-xl mb-3 font-medium">Computer & Software Engineer</p>
          <p className="text-sm max-w-xl mb-6 font-bold">
-            I'm an indie full-stack developer and content creator building my
-            version of the digital world one step at a time. All coding projects
-            are built from the ground up, from planning and designing all the way
-            to solving real-life problems with code.
+            I'm a full-stack developer based in the Philippines, specializing in 
+            building software for the web and mobile. I'm passionate about building 
+            software that makes a difference in the world.
             <br />
-            All video content is built the same way, from ideation and planning,
-            all the way to finalizing the content with artistic touches. I publish
-            that content on my YouTube channel{' '}
+            This is an example link{' '}
             <a
                href="https://youtube.com/fknight"
                target="_blank"
                className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
                rel="noreferrer noopener"
             >
-               ForrestKnight
+               Press me
             </a>{' '}
-            to more than 400k subscribers.
+            
          </p>
       </div>
    )
