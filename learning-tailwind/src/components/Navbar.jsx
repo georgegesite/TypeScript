58b7961c66b9a6ac5b32d96817507f7b72dae8db
George Gesite <georgesite33@gmail.com>
2024-03-16 20:43:34 +0800

    Update Navbar.jsx

--- learning-tailwind/src/components/Navbar.jsx
+++ learning-tailwind/src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 function Navbar() {
   return (
-    <div className="px-10 pt-10">
+    <div className="px-10 pt-10  text-blue-300">
       <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl text-green-400">GEORGE</a>
+        <div className="flex-1 pl-20">
+          <a className=" btn btn-ghost text-xl">George Gesite</a>
         </div>
-        <div className="flex-2">
-          <ul className="menu menu-horizontal px-10">
-            <li>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal text-xl ">
+            <li className="px-10">
               <a>About</a>
             </li>
-            <li>
+            <li className="px-10">
               <a>Project</a>
             </li>
-            <li>
+            <li className="pl-10 pr-32">
               <a>Blog</a>
             </li>
           </ul>
