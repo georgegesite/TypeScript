e5f795e86dfdd1454982b918f041e7b60174eac0
George Gesite <georgesite33@gmail.com>
2024-03-24 13:05:09 +0800

    Update timeline.js

--- learning-tailwind/src/data/timeline.js
+++ learning-tailwind/src/data/timeline.js
@@ -1,37 +1,16 @@
 export default [
   {
     year: "Today",
-    title: "YouTube Content Creator",
-    duration: "6 years",
+    title: " Full Stack Software Engineer",
+    duration: "6 months",
     details:
-      "My humble attempt at building my version of the digital world one step at a time by documenting my coding journey and creating software engineering and computer science content to help those coming behind me.",
+      "I helped in development of additional features for a Japanese Company. Our tech stack consisted of SpringBoot, Thymeleaf, Java, Maven, and a handful of other languages and tools.",
   },
   {
-    year: "Today",
-    title: "Solo Indie Developer",
+    year: "2023",
+    title: "Bachelor's Degree in Computer Engineering",
     duration: "4 years",
     details:
-      "I'm a solo indie developer building software for myself and clients to showcase on YouTube. All coding projects are built from the ground up, from planning and designing all the way to solving real-life problems with code.",
-  },
-  {
-    year: "2019",
-    title: "Software Engineer",
-    duration: "2 years",
-    details:
-      "I helped build an enterprise application for a Fortune 500 company in an Agile team setting. Our tech stack consisted of TypeSciprt, Angular, Java, Gradle, and a handful of other languages and tools.",
-  },
-  {
-    year: "2018",
-    title: "Bachelor's Degree in Computer Science",
-    duration: "5 years",
-    details:
-      "I pursued the knowledge that a computer science degree had to offer. I learned about computer architecture, computer science theory, an array of programming, and aspects of artificial intelligence.",
-  },
-  {
-    year: "2013",
-    title: "First Line of Code",
-    duration: "the beginning",
-    details:
-      "I wrote my first line of real code that started this journey into digital craftsmanship that I never could've imagined, especially as someone who never saw themselves as anything other than a blue collar worker.",
+      "I pursued the knowledge that a computer Engeineering had to offer. I learned about computer architecture and hardware, software engineering, an array of programming, and aspects of artificial intelligence.",
   },
 ];
